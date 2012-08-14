7fc58a3fb20b0ed074061182b3422a9bfb5e63a0
harsh chauhan <harsh@harsh.(none)>
2012-08-14 10:45:10 +0530

    vehclesbclass

--- vehicle_subclass/vehicle.rb
+++ vehicle_subclass/vehicle.rb
@@ -3,13 +3,22 @@ class Vehichle
     @name = name
     @price = price
   end
+  attr_accessor :price
   def show
     puts "#{@name} has a price of #{@price}"
   end
  end
  class Bike < Vehichle
-   def dealer
-   end	
+   def initialize (*info)
+     super info[1], info[2]
+     @dealer = info[0]
+   end
+   def show_bike
+     puts "bike: #{@name}"
+     puts "dealer: #{@dealer}"
+     puts "price: #{@price}"
+   end  
  end
- bike1 = Bike.new("yamaha", 2500)
- bike1.show
+ bike1 = Bike.new("harsh", "yamaha", 2500)
+ bike1.price = 2000
+ bike1.show_bike
