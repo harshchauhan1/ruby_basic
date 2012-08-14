f498e472fbc5506fc9545fdabdbc9d50333a2cdb
harsh chauhan <harsh@harsh.(none)>
2012-08-14 11:51:00 +0530

    caseinverse

--- swap_case/swap_case.rb
+++ swap_case/swap_case.rb
@@ -2,12 +2,9 @@ if __FILE__ == $0
   @str = "harsh CHAUHAN"
 end
 class String
-  def initialize (str)
-  	@str = str
-  end	
-	def to_s
-  puts "#{@str.swapcase}"
-end
+  def to_s
+    puts swapcase
+  end
 end
 str1 = String.new(@str)
 str1.to_s
