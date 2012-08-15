ef29d228aa7db35671297bff887aeb3e6f792c4d
harsh chauhan <harsh@harsh.(none)>
2012-08-16 03:30:56 +0530

    interestdiff

---
+++ interest_diff/interest.rb
@@ -0,0 +1,20 @@
+if __FILE__ == $0
+  @p = 1000
+  @t = 1
+end
+class Interest
+  attr_accessor :t
+  attr_accessor :p
+  attr_accessor :rate
+  def diff
+  	simple =  ((t*p*rate)/100)
+  	compound = p * ((1 + (rate/100.0))**t)
+  	puts "diference is #{compound - simple}"
+  end
+end
+obj = Interest.new.tap do |a|
+  a.p = @p
+  a.t = @t
+  a.rate = 10
+end
+obj.diff
---
+++ interest_diff/main.rb
@@ -0,0 +1,5 @@
+puts "enter principle amount"
+@p = gets.chomp
+puts "enter time period"
+@t = gets.chomp
+require_relative "interest" 
