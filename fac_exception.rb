dabe767fb3fc77478950523308b67c5927fba7a0
harsh chauhan <harsh@harsh.(none)>
2012-08-16 13:13:27 +0530

    facexcptn

---
+++ fac_exception.rb
@@ -0,0 +1,14 @@
+class Integer
+  begin
+  	def factorial
+      f = 1 
+      raise "negative no entered" if self<0
+      for i in 1..self 
+    	f *= i 
+      end 
+      f
+    end
+  end  
+end
+num = -6
+puts num.factorial
