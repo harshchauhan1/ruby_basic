d53afe5f3bc6d9b4972ae491b8e006644a68d772
harsh chauhan <harsh@harsh.(none)>
2012-08-17 09:59:38 +0530

    reviterate

--- reverse_iterate.rb
+++ reverse_iterate.rb
@@ -1,6 +1,11 @@
 class Array
   def reverse_iterate
-    puts self.reverse.join(',')
+    for i in (0..self.length/2-1) do
+      tmp = self[i];
+      self[i] = self[self.length - i - 1];
+      self[self.length - i - 1] = tmp;
+    end
+    puts self.join(' ')
   end
 end
 [2,4,6,8].reverse_iterate {|i| puts "#{i}"}
